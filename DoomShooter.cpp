249332ef4eb3b26dbf9cce6b00a70e1c9362dbd7
Goustern <sinkevich.yaraslau@gmail.com>
2023-07-21 16:19:03 +0300

    wall and new map

--- DoomShooter.cpp
+++ DoomShooter.cpp
@@ -7,7 +7,7 @@
 #include "Base/Screen.h"
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 600
-#define PIXEL_SCALE 1
+#define PIXEL_SCALE 2
 
 struct Math
 {
@@ -50,33 +50,27 @@ class Camera
 public:
     float fov = 200.0;
     Vector3d pos = Vector3d::ZERO;
+    Screen* scr;
+    float a;
     Camera(Screen* scr)
     { 
         this->scr = scr;
         this->pos = Vector3d::ZERO;
     }
-
-    void W_to_S(Vector3d point, int& x, int& y)
+    void WorldPointToScreen(Vector3d point, int& x, int& y)
     {
+        point = Math::RotateAroundPoint3d(point, pos, a);
         point = point - pos;
+
         if (point.z <= 0)
-        {
-            x = 0;
-            y = 0;
             return;
-        }
-        //std::cout << "point.x = " << point.x << '\n';
-        //std::cout << "point.y = " << point.y << '\n';
-        //std::cout << "point.z = " << point.z << '\n';
-        //std::cout << "point.x * fov / point.y = " << point.x * fov / point.y << '\n' << '\n';
-        float a = fov; 
+
+        float a = fov;
         if (point.z != 0)
             a = fov / point.z;
         x = (point.x * a) + scr->GetScreenWidth() / 2;
         y = (-1 * point.y * a) + scr->GetScreenHight() / 2;
     }
-private:
-    Screen* scr;
 };
 Camera* mcam = new Camera(Screen::scr);
 
@@ -114,15 +108,11 @@ public:
         if (a < 0)
             a += 360;
     }
-    void RotateL(float angle)
+    inline Vector3d GetViewDirection()
     {
-        l += angle;
-        if (l >= 360)
-            l -= 360;
-        if (l < 0)
-            l += 360;
+        Vector3d vec = Vector3d(Math::GetSin(this->a), Math::GetSin(this->l), Math::GetCos(this->a));
+        return vec;
     }
-    inline Vector3d GetViewDirection() { return Math::RotateAroundPoint3d(Vector3d::FORWARD, Vector3d::ZERO, 360 - this->a, this->l); }
     void Move()
     {
         float speed = this->speed;
@@ -132,10 +122,6 @@ public:
             RotateA(-1 * rotate_speed * Time::Delta_time());
         if (Input::Get_key_down(SDL_SCANCODE_RIGHT))
             RotateA(rotate_speed * Time::Delta_time());
-        if (Input::Get_key_down(SDL_SCANCODE_UP))
-            RotateL(rotate_speed * Time::Delta_time() / 2);
-        if (Input::Get_key_down(SDL_SCANCODE_DOWN))
-            RotateL(-1 * rotate_speed * Time::Delta_time() / 2);
 
         if (Input::Get_key_down(SDL_SCANCODE_SPACE))
             this->position.y += speed * Time::Delta_time() / 1.3;
@@ -144,7 +130,7 @@ public:
 
         Vector3d vec;
         Vector3d look = this->GetViewDirection();
-        std::cout << look.x << " " << look.y << " " << look.z << '\n';
+        //std::cout << look.x << " " << look.y << " " << look.z << '\n';
         Vector3d par = Math::RotateAroundPoint3d(Vector3d::LEFT, Vector3d::ZERO, 360 - this->a);
 
         if (Input::Get_key_down(SDL_SCANCODE_A)) { vec = vec + par; }
@@ -160,8 +146,54 @@ public:
         std::cout << "y = " << position.y << '\n' << '\n';*/
     }
 };
-Player* p = new Player(Vector3d::ZERO, 0, 0, 2.0f, 15.0f);
+Player* p = new Player(Vector3d(0, 0.5, 0), 0, 0, 2.0f, 15.0f);
 
+class Wall
+{
+public:
+    Vector3d point1, point2, point3, point4;
+    Color col;
+    float height;
+    static Camera* cam;
+    Wall(const Vector3d& point1, const Vector3d& point2, float height, Color col)
+    {
+        init(point1, point2, height, col);
+    }
+    Wall()
+    {
+        this->point1 = Vector3d::ZERO;
+        this->point2 = Vector3d::ZERO;
+        this->point3 = Vector3d::ZERO;
+        this->point4 = Vector3d::ZERO;
+        this->height = 0;
+    }
+    void init(const Vector3d& point1, const Vector3d& point2, float height, Color col)
+    {
+        this->point1 = point1;
+        this->point2 = point2;
+        this->point3 = point1;
+        this->point3.y += height;
+        this->point4 = point2;
+        this->point4.y += height;
+        this->height = height;
+        this->col = col;
+    }
+    void Draw()
+    {
+        int x1, y1, x2, y2;
+        int x3, y3, x4, y4;
+        cam->WorldPointToScreen(point1, x1, y1);
+        cam->WorldPointToScreen(point2, x2, y2);
+        cam->WorldPointToScreen(point3, x3, y3);
+        cam->WorldPointToScreen(point4, x4, y4);
+
+        cam->scr->DrawLine(x1, y1, x2, y2, col);
+        cam->scr->DrawLine(x3, y3, x4, y4, col);
+        cam->scr->DrawLine(x1, y1, x3, y3, col);
+        cam->scr->DrawLine(x2, y2, x4, y4, col);
+    }
+};
+Camera* Wall::cam = mcam;
 class Minimap
 {
 public:
@@ -178,11 +210,11 @@ public:
         this->w = w;
         this->h = h;
     }
-    Vector2d W_to_S(const Vector2d& point)
+    Vector2d WorldPointToScreen(const Vector2d& point)
     {
         return Vector2d(point.x * zoom + w / 2, h / 2 - point.y * zoom);
     }
-    Vector2d W_to_S(const Vector3d& point)
+    Vector2d WorldPointToScreen(const Vector3d& point)
     {
         return Vector2d(point.x * zoom + w / 2, h / 2 - point.z * zoom);
     }
@@ -212,7 +244,7 @@ public:
         scr->DrawRect(1, 1, w, h, Color(0, 0, 0));
 
         Vector2d rotc = Math::RotateAroundPoint2d(Vector2d::ZERO, Vector2d(p->position.x, p->position.z), p->a);
-        rotc = this->W_to_S(rotc);
+        rotc = this->WorldPointToScreen(rotc);
         scr->DrawPixel(rotc.x - p->position.x * zoom, rotc.y + p->position.z * zoom, Color(50, 50, 50));
 
         scr->DrawPixel(w / 2, h / 2, Color(0, 255, 0));
@@ -240,82 +272,69 @@ int main(int argc, char** argv)
     Math::InitSinAndCos();
     Time::Set_target_framerate(120);
 
-    int x1, y1;
-    int x2, y2;
-    int x3, y3;
-    int x4, y4;
     Minimap* mp = new Minimap(100, 60, 4, p, Screen::scr);
 
-    Vector3d point1 = Vector3d(0, -0.5, 1);
-    Vector3d point2 = Vector3d(0, 1, 1);
-    Vector3d point3 = Vector3d(1, -0.5, 1);
-    Vector3d point4 = Vector3d(1, 1, 1);
-    Vector3d point5 = Vector3d(-1, -0.5, 0);
-    Vector3d point6 = Vector3d(-1, 1, 0);
+    const int size = 20;
+    Vector3d point[size];
+    Wall wall[size];
+    point[0] = Vector3d(0.5, 0, 1);
+    point[1] = Vector3d(1, 0, 0.5);
+    point[2] = Vector3d(2, 0, 0.5);
+    point[3] = Vector3d(2, 0, 2);
+    point[4] = Vector3d(3, 0, 2);
+    point[5] = Vector3d(3.5, 0, 2.5);
+    point[6] = Vector3d(4.5, 0, 2.5);
+    point[7] = Vector3d(5, 0, 2);
+    point[8] = Vector3d(6, 0, 2);
+    point[9] = Vector3d(6, 0, -1);
+    point[10] = Vector3d(5, 0, -2);
+    point[11] = Vector3d(3, 0, -2);
+    point[12] = Vector3d(2, 0, -1);
+    point[13] = Vector3d(2, 0, -0.5);
+    point[14] = Vector3d(1, 0, -0.5);
+    point[15] = Vector3d(0.5, 0, -1);
+    point[16] = Vector3d(-1, 0, -1);
+    point[17] = Vector3d(-1.5, 0, -0.5);
+    point[18] = Vector3d(-1.5, 0, 0.5);
+    point[19] = Vector3d(-1, 0, 1);
 
-    mp->points.push_back(&point1);
-    mp->points.push_back(&point2);
-    mp->points.push_back(&point3);
-    mp->points.push_back(&point4);
-    mp->points.push_back(&point5);
-    mp->points.push_back(&point6);
+    for (int i = 0; i < size; i++)
+    {
+        mp->points.push_back(&point[i]);
+        if(i + 1 >= size)
+            wall[i].init(point[i], point[0], 1.5, Color(255, 255, 50));
+        else
+            wall[i].init(point[i], point[i + 1], 1.5, Color(255, 255, 50));
+    }
 
     while (!Input::Qiut())
     {
         Input::Handle_events();
         SDL_SetRenderTarget(Screen::scr->ren, Screen::scr->screen_tex);
-        
         SDL_SetRenderDrawColor(Screen::scr->ren, 0, 0, 0, 255);
         SDL_RenderClear(Screen::scr->ren);
 
+        //////////////////////////////////UPDATE//////////////////////////////////
+
         p->Move();
-
         mcam->pos = p->position;
+        mcam->a = p->a;
 
-        mcam->W_to_S(Math::RotateAroundPoint3d(point1, p->position, p->a, 360 - p->l), x1, y1);
-        mcam->W_to_S(Math::RotateAroundPoint3d(point2, p->position, p->a, 360 - p->l), x2, y2);
-        mcam->W_to_S(Math::RotateAroundPoint3d(point3, p->position, p->a, 360 - p->l), x3, y3);
-        mcam->W_to_S(Math::RotateAroundPoint3d(point4, p->position, p->a, 360 - p->l), x4, y4);
-        Screen::scr->DrawLine(x1, y1, x2, y2, Color(255, 0, 0));
-        Screen::scr->DrawLine(x3, y3, x4, y4, Color(255, 0, 0));
-        Screen::scr->DrawLine(x1, y1, x3, y3, Color(255, 0, 0));
-        Screen::scr->DrawLine(x2, y2, x4, y4, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint3d(point5, p->position, p->a, 360 - p->l), x3, y3);
-        mcam->W_to_S(Math::RotateAroundPoint3d(point6, p->position, p->a, 360 - p->l), x4, y4);
-        Screen::scr->DrawLine(x1, y1, x2, y2, Color(255, 0, 0));
-        Screen::scr->DrawLine(x3, y3, x4, y4, Color(255, 0, 0));
-        Screen::scr->DrawLine(x1, y1, x3, y3, Color(255, 0, 0));
-        Screen::scr->DrawLine(x2, y2, x4, y4, Color(255, 0, 0));
-        /*Screen::scr->DrawPixel(x, y, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint(point2, p->position, p->a), x, y);
-        Screen::scr->DrawPixel(x, y, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint(point3, p->position, p->a), x, y);
-        Screen::scr->DrawPixel(x, y, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint(point4, p->position, p->a), x, y);
-        Screen::scr->DrawPixel(x, y, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint(point5, p->position, p->a), x, y);
-        Screen::scr->DrawPixel(x, y, Color(255, 0, 0));
-        mcam->W_to_S(Math::RotateAroundPoint(point6, p->position, p->a), x, y);
-        Screen::scr->DrawPixel(x, y, Color(255, 0, 0));*/
+        for (int i = 0; i < size; i++)
+        {
+            wall[i].Draw();
+        }
+
         mp->zoom += Input::Get_scroll_whell() * Time::Delta_time() * 10;
-        //std::cout << mcam->fov << '\n';
-        /*std::cout << "x cam = " << mcam->pos.x << '\n';
-        std::cout << "y cam = " << mcam->pos.y << '\n';
-        std::cout << "z cam = " << mcam->pos.z << '\n';
-        std::cout << "x1 = " << x1 << '\n';
-        std::cout << "y1 = " << y1 << '\n';
-        std::cout << "x2 = " << x2 << '\n';
-        std::cout << "y2 = " << y2 << '\n';*/
-
         std::cout << 1.0f / Time::Delta_time() << '\n';
 
         mp->Draw2();
 
-        SDL_SetRenderTarget(Screen::scr->ren, NULL);
+        //////////////////////////////////UPDATE//////////////////////////////////
 
+        SDL_SetRenderTarget(Screen::scr->ren, NULL);
         SDL_RenderCopy(Screen::scr->ren, Screen::scr->screen_tex, NULL, NULL);
         SDL_RenderPresent(Screen::scr->ren);
-
         Time::Tick();
     }
     Screen::scr->ApplicationQuit();
